ede120f5b4d20e334ff1cd2437a360d71fa8b38d
Renata Sabrina Messias <268029922+messiasmss-renata@users.noreply.github.com>
2026-08-15 17:48:13 -0300

    Add files via upload

---
+++ processacadastro.php
@@ -0,0 +1,14 @@
+<?php 
+$Nome = $_POST["Nome"];
+echo "Olá $Nome! Seu Cadastro foi realizado com sucesso!"."<br>";
+
+echo "Nome: ".$_POST["Nome"]."<br>";
+
+echo "Idade: ".$_POST["Idade"]."<br>";
+
+echo "Profissao: ".$_POST["Profissao"]."<br>";
+
+echo "Salario: ".$_POST["Salario"]."<br>";
+
+echo "Experiencia: ".$_POST["Experiencia"]."<br>";
+?>
